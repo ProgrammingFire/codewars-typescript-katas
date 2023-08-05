e64c8f7eda4b9e7d20b4e9218f83526c3a5e62d4
Nouman Rahman <techcode786@gmail.com>
2023-08-05 12:52:35 +0530

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export function maskify(cc: string): string {
+  if (cc.length >= 5) {
+    return "#".repeat(cc.length - 4) + cc.slice(cc.length - 4, cc.length)
+  }
+  return cc
+}
+
+console.log(maskify("12345"))
