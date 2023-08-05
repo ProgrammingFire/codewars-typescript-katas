489708fc015d15f9cf3b0f3d4dfccabf4c502f3a
Nouman Rahman <techcode786@gmail.com>
2023-08-05 12:53:11 +0530

    Add narcissistic number kata

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,20 @@ export function maskify(cc: string): string {
   return cc
 }
 
+export function narcissistic(value: number): boolean {
+  let narNum: number = value
+  let narStr: string = narNum.toString()
+
+  let result: number = 0;
+
+  for (let index = 0; index < narStr.length; index++) {
+    let num = parseInt(narStr[index])
+    result = result + Math.pow(num, narStr.length)
+  }
+
+
+  return result == value
+}
+
 console.log(maskify("12345"))
+console.log(narcissistic(153))
