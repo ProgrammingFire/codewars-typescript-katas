d5c4d99a9fec5be1e10aba072846b8659451ed43
Nouman Rahman <techcode786@gmail.com>
2023-08-10 17:37:35 +0530

    Uncomment the other lines

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,49 @@
-// import { maskify } from "./maskify"
-// import { narcissistic } from "./narcissistic"
-// import { descendingOrder } from "./descending-order"
-// import { isPangram } from "./detect-pangram";
-// import { comp } from "./are-they-same"
-// import { numberOfPeople } from "./bus-stop"
-// import { removeNb } from "./is-my-friend-cheating";
-// import { findOdd } from "./find-the-odd-int";
-// import { findOutlier } from "./parity-outlier"
-// import { nbMonths } from "./buying-a-car";
-// import { findUniq } from "./unique-string";
-// import { wave } from "./mexican-wave";
-// import { makeLooper } from "./lazy-repeater";
-// import { perimeter } from "./perimeter-of-squares";
-// import { closest } from "./closest-and-smallest";
-// import { sumPairs } from "./sum-of-pairs";
+import { maskify } from "./maskify"
+import { narcissistic } from "./narcissistic"
+import { descendingOrder } from "./descending-order"
+import { isPangram } from "./detect-pangram";
+import { comp } from "./are-they-same"
+import { numberOfPeople } from "./bus-stop"
+import { removeNb } from "./is-my-friend-cheating";
+import { findOdd } from "./find-the-odd-int";
+import { findOutlier } from "./parity-outlier"
+import { nbMonths } from "./buying-a-car";
+import { findUniq } from "./unique-string";
+import { wave } from "./mexican-wave";
+import { makeLooper } from "./lazy-repeater";
+import { perimeter } from "./perimeter-of-squares";
+import { closest } from "./closest-and-smallest";
+import { sumPairs } from "./sum-of-pairs";
 import { playPass } from "./playing-with-passphrases";
 
-// console.log(maskify("12345"))
-// console.log(narcissistic(153))
-// console.log(descendingOrder(5415))
+console.log(maskify("12345"))
+console.log(narcissistic(153))
+console.log(descendingOrder(5415))
 
-// let a = [121, 144, 19, 161, 19, 144, 19, 11]
-// let b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
-// console.log(comp(a, b))
+let a = [121, 144, 19, 161, 19, 144, 19, 11]
+let b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+console.log(comp(a, b))
 
-// console.log(numberOfPeople([[10, 0], [3, 5], [5, 8]]))
+console.log(numberOfPeople([[10, 0], [3, 5], [5, 8]]))
 
-// console.log(isPangram("The quick brown fox jumps over the lazy dog."))
-// console.log(isPangram("This is not a pangram."))
+console.log(isPangram("The quick brown fox jumps over the lazy dog."))
+console.log(isPangram("This is not a pangram."))
 
-// console.log(removeNb(26))
-// console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
-// console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
-// console.log(nbMonths(2000, 8000, 1000, 1.5))
-// console.log(findUniq(['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']))
-// console.log(wave("hello"))
+console.log(removeNb(26))
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
+console.log(nbMonths(2000, 8000, 1000, 1.5))
+console.log(findUniq(['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']))
+console.log(wave("hello"))
 
-// const abc = makeLooper('abc');
-// console.log(abc())
-// console.log(abc())
-// console.log(abc())
-// console.log(abc())
-// console.log(abc())
+const abc = makeLooper('abc');
+console.log(abc())
+console.log(abc())
+console.log(abc())
+console.log(abc())
+console.log(abc())
 
-// console.log(perimeter(5))
-// console.log(closest("103 123 4444 99 2000"))
-// console.log(sumPairs([11, 3, 7, 5], 10))
+console.log(perimeter(5))
+console.log(closest("103 123 4444 99 2000"))
+console.log(sumPairs([11, 3, 7, 5], 10))
 console.log(playPass("BORN IN 2015!", 1))
